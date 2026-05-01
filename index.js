2641a81288783e3845dc7882c28e9cd524b6e6b5
星野栄治 <177436503+HosinoEJ@users.noreply.github.com>
2026-05-01 21:44:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,8 +44,13 @@ app.post('/api/webhook', async (req, res) => {//github webhook api處理
                 .filter(Boolean)
                 .map(path => path.split('/').pop()); // 這裡把路徑修掉
         };
-        const added = added_files.map(file => formatFiles(file));
-        const modified = modified_files.map(file => formatFiles(file));
+        if(Array.isArray(added_files) && added_files.length > 0){
+            const added = added_files.map(file => formatFiles(file));
+        }
+        if(Array.isArray(modified_files) && modified_files.length > 0){
+            const modified = modified_files.map(file => formatFiles(file));
+        }
+        
 
         const commitUrl = compare || "https://github.com/HosinoEJ/HosinoNeko-SUB-SEND";
 
